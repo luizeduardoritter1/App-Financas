94649b1b2840d961080b360f74e20b08a4dc5a94
Luiz Eduardo Ritter Leal <luizeduardoritter6@gmail.com>
2024-07-06 04:13:03 -0300

    commit initial

---
+++ src/main/java/br/com/meuapp/HomeWealth/HomeWealthApplication.java
@@ -0,0 +1,13 @@
+package br.com.meuapp.HomeWealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/meuapp/HomeWealth/HomeWealthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meuapp.HomeWealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
